7960900b6946be2eb57b89bf5cf7b348b46c3b12
Placide IRANDORA <placideirandora@gmail.com>
2020-10-12 20:02:22 +0200

    Register the Route for the Sign Up page

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,7 +2,10 @@ const routes = [
   {
     path: "/",
     component: () => import("layouts/MainLayout.vue"),
-    children: [{ path: "", component: () => import("pages/Index.vue") }]
+    children: [
+      { path: "", component: () => import("pages/SignIn.vue") },
+      { path: "/register", component: () => import("pages/SignUp.vue") },
+    ]
   },
 
   // Always leave this as last one,
