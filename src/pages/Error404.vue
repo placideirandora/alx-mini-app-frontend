73fa2558d57691c1a647982084667502ae1bbdbf
Placide IRANDORA <placideirandora@gmail.com>
2020-10-13 19:51:52 +0200

    Change the Background and Redirect the user to the Homepage or Login page accordingly

--- src/pages/Error404.vue
+++ src/pages/Error404.vue
@@ -1,27 +1,45 @@
 <template>
   <div
-    class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center"
+    class="fullscreen bg-red text-white text-center q-pa-md flex flex-center"
   >
     <div>
       <div style="font-size: 30vh">404</div>
 
-      <div class="text-h2" style="opacity: 0.4">Oops. Nothing here...</div>
+      <div class="text-h2" style="opacity: 0.4">Oops. Page Not Found.</div>
 
       <q-btn
         class="q-mt-xl"
         color="white"
-        text-color="blue"
+        text-color="red"
         unelevated
-        to="/"
-        label="Go Home"
+        label="Go Back Home"
         no-caps
+        @click="onGoBackHome"
       />
     </div>
   </div>
 </template>
 
 <script>
+import { mapGetters, mapActions } from "vuex";
+
 export default {
   name: "Error404",
+  computed: {
+    ...mapGetters(["loggedIn"]),
+  },
+  methods: {
+    ...mapActions(["checkLoginStatus"]),
+    onGoBackHome() {
+      if (this.loggedIn) {
+        this.$router.push({ name: "home" });
+      } else {
+        this.$router.push({ name: "login" });
+      }
+    },
+  },
+  created() {
+    this.checkLoginStatus();
+  },
 };
 </script>
