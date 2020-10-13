b91f223c7fd996f9428f3188edff559867f929cc
Placide IRANDORA <placideirandora@gmail.com>
2020-10-13 14:16:44 +0200

    Setup Vuex for State Management of the App

---
+++ src/store/user_module/getters.js
@@ -0,0 +1,5 @@
+export const user = state => state.user;
+
+export const loggedIn = state => state.loggedIn;
+
+export const loading = state => state.loading;
---
+++ src/store/user_module/mutations.js
@@ -0,0 +1,11 @@
+export const setLoading = (state, payload) => {
+  state.loading = payload;
+};
+
+export const setUser = (state, payload) => {
+  state.user = payload;
+};
+
+export const setLoggedIn = (state, payload) => {
+  state.loggedIn = payload;
+};
