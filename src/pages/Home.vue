8cb2e2774446762cc25ca64f0b743069b78dfa0e
Placide IRANDORA <placideirandora@gmail.com>
2020-10-13 14:18:22 +0200

    Create Initial Homepage

---
+++ src/pages/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page class="flex flex-center">
+    <p class="text-h3">Welcome to the Homepage</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "PageIndex",
+};
+</script>
