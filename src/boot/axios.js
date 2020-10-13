34d8fbe4aeede58a87923db987d4c2aabe798b52
Placide IRANDORA <placideirandora@gmail.com>
2020-10-13 14:15:16 +0200

    Configure Axios and make it accessible in the Store as well

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,4 +1,10 @@
 import Vue from "vue";
 import axios from "axios";
 
-Vue.prototype.$axios = axios;
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:3000/api/v1"
+});
+
+Vue.prototype.$axios = axiosInstance;
+
+export default axiosInstance;
