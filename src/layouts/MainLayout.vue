0830989630ee6a19c0904f72790ad4ec73fac254
Placide IRANDORA <placideirandora@gmail.com>
2020-10-14 17:21:02 +0200

    Add more menus and display username along a default Avatar

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -12,6 +12,42 @@
         />
 
         <q-toolbar-title> ALX MINI APP </q-toolbar-title>
+
+        <div v-if="loggedIn">
+          <q-chip
+            clickable
+            @click="showDropdownMenu = !showDropdownMenu"
+            :color="$q.screen.xs ? 'transparent' : 'white'"
+            :ripple="$q.screen.xs ? false : true"
+          >
+            <q-avatar>
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+            </q-avatar>
+            {{ !this.$q.screen.xs ? user.userName : null }}
+          </q-chip>
+        </div>
+
+        <div v-if="loggedIn">
+          <q-menu
+            :offset="[0, 28]"
+            v-model="showDropdownMenu"
+            fit
+            anchor="bottom left"
+            self="top left"
+          >
+            <q-item clickable>
+              <q-item-section>Profile</q-item-section>
+            </q-item>
+            <q-separator />
+            <q-item clickable>
+              <q-item-section>Change Password</q-item-section>
+            </q-item>
+            <q-separator />
+            <q-item clickable @click="onSignOut">
+              <q-item-section>Sign Out</q-item-section>
+            </q-item>
+          </q-menu>
+        </div>
       </q-toolbar>
     </q-header>
 
@@ -28,6 +64,30 @@
 
       <q-list v-if="loggedIn">
         <q-item-label header class="text-grey-8"> Quick Menu </q-item-label>
+        <q-item clickable>
+          <q-item-section avatar>
+            <q-icon name="account_box" />
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label>Profile</q-item-label>
+            <q-item-label caption> Go to the Profile Page </q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable>
+          <q-item-section avatar>
+            <q-icon name="lock" />
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label>Change Password</q-item-label>
+            <q-item-label caption>
+              Go to the Change Password Page
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+
         <q-item clickable @click="onSignOut">
           <q-item-section avatar>
             <q-icon :name="authMenus[0].icon" />
@@ -85,16 +145,17 @@ export default {
     leftDrawerOpen: false,
     menus: menuData,
     authMenus: authMenuData,
+    showDropdownMenu: false,
   }),
   computed: {
-    ...mapGetters(["loggedIn"]),
+    ...mapGetters(["loggedIn", "user"]),
   },
   methods: {
     ...mapActions(["checkLoginStatus", "signUserOut"]),
     onSignOut() {
       this.signUserOut();
 
-      this.$router.push({ path: "/" });
+      this.$router.push({ name: "login" });
     },
   },
   created() {
