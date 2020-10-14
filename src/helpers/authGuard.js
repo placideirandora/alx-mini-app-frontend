993d0d18fea6df437a80d4e8cc11603d5aef1d3c
Placide IRANDORA <placideirandora@gmail.com>
2020-10-14 19:01:45 +0200

    Authenticate the Profile Page as well

--- src/helpers/authGuard.js
+++ src/helpers/authGuard.js
@@ -5,8 +5,11 @@ import store from "../store/index";
 export default (to, from, next) => {
   store.dispatch("checkLoginStatus");
 
-  if ((!store.getters.loggedIn && to.name === "home") ||
-   (!store.getters.loggedIn && to.name === "change-password")) {
+  if (
+    (!store.getters.loggedIn && to.name === "home") ||
+    (!store.getters.loggedIn && to.name === "change-password") ||
+    (!store.getters.loggedIn && to.name === "profile")
+  ) {
     Notify.create({
       type: "negative",
       message: "You must be signed in to access the page"
