fb0167349d5555dcbfb909d7c1eec267d63a73f0
Placide IRANDORA <placideirandora@gmail.com>
2020-10-16 01:19:39 +0200

    Use the Production API Version

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api/v1"
+  baseURL: "https://alx-mini-app-api.herokuapp.com/api/v1"
 });
 
 Vue.prototype.$axios = axiosInstance;
