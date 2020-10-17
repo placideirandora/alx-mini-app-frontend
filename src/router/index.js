e147e38ecb244f9cb47f24c36f911394ec9f5fc2
Placide IRANDORA <placideirandora@gmail.com>
2020-10-17 12:00:11 +0200

    Configure the Router to be used outside Components as well

--- src/router/index.js
+++ src/router/index.js
@@ -14,17 +14,13 @@ Vue.use(VueRouter);
  * with the Router instance.
  */
 
-export default function(/* { store, ssrContext } */) {
-  const Router = new VueRouter({
-    scrollBehavior: () => ({ x: 0, y: 0 }),
-    routes,
+export default new VueRouter({
+  scrollBehavior: () => ({ x: 0, y: 0 }),
+  routes,
 
-    // Leave these as they are and change in quasar.conf.js instead!
-    // quasar.conf.js -> build -> vueRouterMode
-    // quasar.conf.js -> build -> publicPath
-    mode: process.env.VUE_ROUTER_MODE,
-    base: process.env.VUE_ROUTER_BASE
-  });
-
-  return Router;
-}
+  // Leave these as they are and change in quasar.conf.js instead!
+  // quasar.conf.js -> build -> vueRouterMode
+  // quasar.conf.js -> build -> publicPath
+  mode: process.env.VUE_ROUTER_MODE,
+  base: process.env.VUE_ROUTER_BASE
+});
